469bd91915041b3e26e88f1eb0c8f9b8c5700606
SAMBO <hdservice@DESKTOP-3JK6ANU>
2025-03-05 16:30:52 +0200

    -Add Catrgories Controller -Add Catrgories Index view

---
+++ Booky.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Booky.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booky.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            //TODO : Use viewModel
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
